24da2977b0539980cc6c8a5451f22e3e499ced9c
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2019-12-30 19:50:27 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var menuButton = document.querySelector('.menu-button');
+var navigationMenu = document.querySelector('.site-navigation')
+var open = false;
+menuButton.addEventListener('click', function(){
+    if(open){
+        menuButton.classList.remove('x');
+        navigationMenu.classList.remove('open');
+        open = false;
+        return;
+    }
+    menuButton.classList.add('x');
+    navigationMenu.classList.add('open');
+    open = true;
+    console.log(menuButton, navigationMenu);
+    return;
+});
