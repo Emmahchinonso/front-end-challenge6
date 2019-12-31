15458fe7fbe9fabe5fd24821101020b10b084a25
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2019-12-31 18:04:25 +0100

    made other options to close menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var menuButton = document.querySelector('.menu-button');
 var navigationMenu = document.querySelector('.site-navigation')
 var open = false;
-menuButton.addEventListener('click', function(){
+menuButton.addEventListener('click', function(e){
+    e.stopPropagation();
     if(open){
         menuButton.classList.remove('x');
         navigationMenu.classList.remove('open');
@@ -14,3 +15,12 @@ menuButton.addEventListener('click', function(){
     console.log(menuButton, navigationMenu);
     return;
 });
+
+document.body.addEventListener('click', function(){
+    if(open){
+        menuButton.classList.remove('x');
+        navigationMenu.classList.remove('open');
+        open = false;
+        return;
+    }
+})
